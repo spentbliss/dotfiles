af16a472b59e7f942e6ee9b3cce2a433d3f560cd
spentbliss <spentbliss@users.noreply.github.com>
2025-05-26 00:07:15 -0700

    chore: remove unused code & add raycast package

--- setup.sh
+++ setup.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-# Check if the user has sudo permissions
-# if [ "$(id -u)" -ne 0 ]; then
-#     echo "This script must be run with sudo."
-#     exit 1  
-# fi
-
 read -p "Enter your username: " USERNAME
-
-# # Check if the Command Line Tools are installed
-# if ! xcode-select -p &> /dev/null; then
-#     echo "CLI Tools for Xcode are not installed. Installing..."
-#     xcode-select --install
-
-#     echo "Please complete the installation of Command Line Tools."
-    
-#     # Wait for the installation to complete
-#     while ! xcode-select -p &> /dev/null; do
-#         echo "Sleeping for 5 seconds just wait for the install to complete."
-#         sleep 5 # Checking every 5 seconds if xcode tools are installed.
-#     done
-
-#     echo "Command Line Tools installed successfully."
-# fi
 
 # Install Homebrew if it's not already installed
 if ! command -v brew &> /dev/null; then
@@ -60,8 +38,7 @@ brew analytics off
 
 # Install programs and fonts
 echo "Installing programs and fonts..."
-brew install typescript node typescript-language-server vscode-langservers-extracted
-brew install btop bat helix eza
+brew install typescript node typescript-language-server vscode-langservers-extracted btop bat helix eza
 brew install --cask kitty alt-tab nikitabobko/tap/aerospace keepassxc raycast 
 
 # Installing Rust if it's not already installed
