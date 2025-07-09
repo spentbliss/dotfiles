7d97e55cf7b5f7a78fe76384f7bc53d41e606363
spentbliss <206476473+spentbliss@users.noreply.github.com>
2025-07-08 20:01:12 -0700

    chore: update settings.lua

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -11,3 +11,21 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.smartindent = true
+
+-- Highlighting stuff 
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  pattern = "/,?",
+  callback = function()
+    vim.schedule(function()
+      vim.o.hlsearch = false
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  pattern = "/,?",
+  callback = function()
+    vim.o.hlsearch = true
+  end,
+})
+
