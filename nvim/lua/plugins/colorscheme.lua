224c305d523a62aef339c2be303599dd7d1d9e32
spentbliss <206476473+spentbliss@users.noreply.github.com>
2025-07-08 20:02:09 -0700

    chore: remove unused colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "vague2k/vague.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
     "decaycs/decay.nvim",
     lazy = false,
     priority = 1000,
