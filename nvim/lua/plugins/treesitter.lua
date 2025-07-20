ed4db3072984b8742327a28e8335d3af9b01fe3b
spentbliss <spentbliss@users.noreply.github.com>
2025-07-20 12:39:41 -0700

    chore: fix delay by loading lazily

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,33 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "python", "javascript", "bash", "json" },
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	priority = 1000,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "python", "c", "cpp", "rust", "bash" },
 
-      highlight = {
-        enable = true,
-        -- Disable treesitter for big files
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-          return false
-        end,
-        additional_vim_regex_highlighting = false,
-      },
+			highlight = {
+				enable = true,
+				-- Disable treesitter for big files
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+					return false
+				end,
+				additional_vim_regex_highlighting = false,
+			},
 
-      indent = {
-        enable = true,
-      },
+			indent = {
+				enable = true,
+			},
 
-      incremental_selection = {
-        enable = true,
-      },
-    })
-  end,
+			incremental_selection = {
+				enable = true,
+			},
+		})
+	end,
 }
-
