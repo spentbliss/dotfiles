dc6a7a0cfcbc3328f5f82d71a3de3e58911d3d59
spentbliss <spentbliss@users.noreply.github.com>
2025-07-20 12:42:56 -0700

    chore: add markdown treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,12 +5,12 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "python", "c", "cpp", "rust", "bash" },
+			ensure_installed = { "markdown", "markdown_inline", "lua", "python", "c", "cpp", "rust", "bash" },
 
 			highlight = {
 				enable = true,
 				-- Disable treesitter for big files
-				disable = function(lang, buf)
+				disable = function(_, buf)
 					local max_filesize = 100 * 1024 -- 100 KB
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
